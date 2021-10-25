eb25ea4fee96d24cb13fc86b84495a1ecf4fcd8c
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-10-24 22:44:22 -0400

    update tag route using category route as a reference

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -45,6 +45,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async  (req, res) => {
   // create a new category
   try {
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -80,7 +81,7 @@ router.delete('/:id', async  (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found !' });
+      res.status(404).json({ message: 'No category found!' });
       return;
     }
 
